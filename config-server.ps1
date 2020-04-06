40ad3ccff5178a737d4b5c1ba08b1a5744e6c9ef
Cansın Aldanmaz <canald@gmail.com>
2020-04-06 23:46:27 +0300

    Update config-server.ps1
    remove comments

--- config-server.ps1
+++ config-server.ps1
@@ -24,44 +24,11 @@ Start-PodeServer -Threads $env:ThreadCount {
     New-PodeLoggingMethod -Terminal | Enable-PodeRequestLogging
     Restore-PodeState -Path './state.json'
     Add-PodeEndpoint -Address * -Port $env:PodePort -Protocol Http
-    # Add-PodeSchedule -Name 'save-state' -Cron '@minutely' -ScriptBlock {
-    #     param($e)
-    #     #git clone https://github.com/haidouks/configs.git configurations 
-    #     $testConfigs = Get-Content -Path ./configurations/test.yaml -Raw | ConvertFrom-Yaml
-    #     $testConfigs | Out-String | Out-PodeHost
-    #     Lock-PodeObject -Object $e.Lockable {
-    #         Set-PodeState -Name 'test' -Value @{Name="ali"} | Out-Null
-    #         "In lock" | out-podehost
-    #         Get-PodeState -Name 'test'| Out-String | Out-PodeHost
-    #         Save-PodeState -Path '/Users/cansinaldanmaz/Downloads/workspace/github.com/haidouks/config-server/state.json' -Verbose
-    #     } -verbose
-    #     "Get state" | Out-PodeHost
-    #     Get-PodeState -Name 'test'| Out-String | Out-PodeHost
-    #     "Saving kjhaskdad" | Out-PodeHost
-    # }
+    
     Add-PodeSchedule -Name 'get-config' -Cron '@minutely' -FilePath ./schedules/get-configs.ps1
     Add-PodeSchedule -Name 'get-routes' -Cron '@minutely' -FilePath ./schedules/get-routes.ps1
     Add-PodeSchedule -Name 'new-routes' -Cron '@minutely' -FilePath ./schedules/new-routes.ps1
-    #    param($e)
-    #    $VerbosePreference = "Continue"
-    #    try { 
-    #        "Clonning repository" | Out-PodeHost
-    #        git clone https://github.com/haidouks/configs.git configurations | Out-PodeHost
-    #        $testConfigs = Get-Content -Path ./configurations/test.yaml -Raw | ConvertFrom-Yaml -AllDocuments
-    #        "Saving:`n$($testConfigs|out-String)" | Out-PodeHost
-    #        # ensure we're thread safe
-    #        Lock-PodeObject -Object $e.Lockable {
-    #            "Getting State11:" | Out-PodeHost
-    #            Set-PodeState -Name 'test' -Value @{ 'Name' = 'Rick Sanchez' }| Out-Null
-    #            Save-PodeState -Path './state.json'
-    #            "Getting State:" | Out-PodeHost
-    #        }
-    #        "Disarda Getting State" | Out-PodeHost
-    #        }
-    #    catch {
-    #        throw $_
-    #    }
-    #}
+    
     Add-PodeRoute -Method Get -Path '/ping' -ScriptBlock {
         Write-PodeJsonResponse -Value @{Status = "Healthy"}
     }
