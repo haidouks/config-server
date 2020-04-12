105b0c40933d49cabc603bcabbeea0ea89347007
Cansın Aldanmaz <caldanmaz@ziraatteknoloji.com>
2020-04-12 15:57:39 +0300

    remove unsused config files

--- schedules/get-configs.ps1
+++ schedules/get-configs.ps1
@@ -19,8 +19,18 @@
         }
 
         $configFiles = Get-ChildItem -Path $configPath -Filter *.yaml | Select-Object Name,FullName
+        $exConfigFiles = $null
         Lock-PodeObject -Object $Event.Lockable {
+            $exConfigFiles = Get-PodeState -Name configFiles
             Set-PodeState -Name configFiles -Value $configFiles.Name | Out-Null
+        }
+        foreach($exConfigFile in $exConfigFiles) {
+            if($configFiles.Name -notcontains $exConfigFile) {
+                Write-Warning -Message "$fileName __ Removing unused config state $exConfigFile"
+                Lock-PodeObject -Object $Event.Lockable {
+                    Remove-PodeState -Name $exConfigFile 
+                }
+            }
         }
 
         foreach($configFile in $configFiles) {
