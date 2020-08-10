3e32859051aee4af967da9cf25a0fbfd54326082
Cansın Aldanmaz <canald@gmail.com>
2020-08-10 18:46:08 +0300

    Update get-repo.ps1

--- schedules/get-repo.ps1
+++ schedules/get-repo.ps1
@@ -5,6 +5,7 @@
         $ProgressPreference = "SilentlyContinue"
         $VerbosePreference = $env:VerbosePreference ? $env:VerbosePreference : "SilentlyContinue"
         
+        $env:GIT_REDIRECT_STDERR = '2>&1'
         $fileName = "get-repo"
         $env:repo ??= "https://github.com/haidouks/configs.git"
         $configPath = Join-Path -Path (Get-PodeServerPath) -ChildPath "configs"
@@ -14,7 +15,7 @@
 
         if(Test-Path $repoPath) {
             Write-Verbose -Message "$(Get-Date -Format "yyyyMMddHHmmssfff") $fileName __ Starting to pull changes from $env:repo to: $repoPath"
-            git -C $repoPath pull 2>/dev/null
+            git -C $repoPath pull
             if ($LASTEXITCODE) { 
                 Throw "$(Get-Date -Format "yyyyMMddHHmmssfff") Unable to pull repo: $($env:repo), exit code: $LASTEXITCODE" 
             }
